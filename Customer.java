bac6ded9fb4e3226d34c2dcdec96b6dfbeab9c6b
Rommel Aquino Jr <rommelaquinojr@Omniscient-Tombstone.local>
2023-10-27 21:59:54 -0700

    added setPhoneNumber(double pn) method

--- Customer.java
+++ Customer.java
@@ -4,8 +4,24 @@ public class Customer {
     private String name;
     private String reservationID;
 
+
     public void searchRestaurant() {}
+
+
     public void addReservation() {}
+    
+    
     public void editReservation() {}
+    
+    
     public void cancelReservation() {}
+
+
+    /**
+     * Sets the phone number of the customer object
+     * @param pn phone number
+     */
+    public void setPhoneNumber(double pn) {
+        this.phoneNumber = pn;
+    }
 }
